9c438c49fe62befb0ad8274022f36f7f60952b45
MagisterLeon <magisterleoni@gmail.com>
2022-08-23 13:40:49 +0200

    JEQB-82: dev p2p network implementation (#3)
    * JEQB-82: dev p2p network implementation
    
    * JEQB-82: formatting
    
    Co-authored-by: Łukasz Kujawski <leon@wildland.io>

---
+++ crates/communication/p2p/src/config.rs
@@ -0,0 +1,5 @@
+/// P2P service configuration
+pub struct P2PConfiguration {
+    /// IP address to listen for incoming connections.
+    pub listen_address: String,
+}
--- crates/quantum-metachain/src/main.rs
+++ crates/quantum-metachain/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello Peers!")
+use qmc_p2p::config::P2PConfiguration;
+use qmc_p2p::service::{DevP2PService, P2PService};
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let p2p_config = P2PConfiguration {
+        // TODO JEQB-79 read listen address from config
+        listen_address: "/ip4/0.0.0.0/tcp/0".to_string(),
+    };
+
+    let p2p_service = DevP2PService::new(p2p_config);
+    match p2p_service.start().await {
+        Ok(_) => {}
+        Err(err) => panic!("Cannot start p2p service: {:?}", err),
+    };
+    Ok(())
 }
