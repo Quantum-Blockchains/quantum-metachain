389b36c0e83f43c9f6839cdca0579854b7da391e
Łukasz Kujawski <leon@wildland.io>
2022-08-23 08:30:02 +0200

    JEQB-82: dev p2p network implementation

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Quantum Meta Chain
+
+## Configuration
+
+## Run
+```
+cargo run
+```
+## Docker
+```
+docker build -t quantum-metachain .
+docker run -it quantum-metachain
+```
+
+## Test
+
+### Unit tests
+```
+cargo test
+```
+
+## Documentation
+
---
+++ crates/communication/p2p/src/service.rs
@@ -0,0 +1,61 @@
+use async_trait::async_trait;
+use std::error::Error;
+
+use crate::config::P2PConfiguration;
+use libp2p::futures::StreamExt;
+use libp2p::identity::Keypair;
+use libp2p::mdns::{Mdns, MdnsConfig, MdnsEvent};
+use libp2p::swarm::SwarmEvent;
+use libp2p::{PeerId, Swarm};
+
+#[async_trait]
+pub trait P2PService {
+    async fn start(self) -> Result<(), Box<dyn Error>>;
+}
+
+pub struct DevP2PService {
+    config: P2PConfiguration,
+    id_keys: Keypair,
+}
+
+impl DevP2PService {
+    pub fn new(config: P2PConfiguration) -> DevP2PService {
+        let id_keys = Keypair::generate_ed25519();
+        DevP2PService { config, id_keys }
+    }
+}
+
+#[async_trait]
+impl P2PService for DevP2PService {
+    // TODO JEQB-80 provide error as enum with use of thiserror lib and replace `Box<dyn Error>`
+    async fn start(self) -> Result<(), Box<dyn Error>> {
+        let transport = libp2p::development_transport(self.id_keys.clone()).await?;
+        let behaviour = Mdns::new(MdnsConfig::default()).await?;
+        let peer_id = PeerId::from(self.id_keys.public());
+
+        let mut swarm = Swarm::new(transport, behaviour, peer_id);
+        swarm.listen_on(self.config.listen_address.parse()?)?;
+
+        loop {
+            match swarm.select_next_some().await {
+                SwarmEvent::NewListenAddr { address, .. } => {
+                    // TODO JEQP-78 implement logging instead of printlns
+                    println!("Listening on local address {:?}", address)
+                }
+                SwarmEvent::Behaviour(MdnsEvent::Discovered(peers)) => {
+                    for (peer, addr) in peers {
+                        println!("discovered {} {}", peer, addr);
+                        // TODO JEQB-81 count all peers
+                    }
+                }
+                SwarmEvent::Behaviour(MdnsEvent::Expired(expired)) => {
+                    for (peer, addr) in expired {
+                        println!("expired {} {}", peer, addr);
+                    }
+                }
+                // TODO JEQB-81 log when a peer leaves the network
+                _ => {}
+            }
+        }
+    }
+}
