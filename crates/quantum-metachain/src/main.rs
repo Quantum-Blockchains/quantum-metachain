b8761eb6d081aa621bd2d2b18360a8d4546818f5
Wolmin <lamonos123@gmail.com>
2022-08-23 15:48:08 +0200

    Adjust cargo fmt

--- crates/quantum-metachain/src/main.rs
+++ crates/quantum-metachain/src/main.rs
@@ -5,11 +5,14 @@ use qmc_p2p::service::{DevP2PService, P2PService};
 async fn main() -> std::io::Result<()> {
     let p2p_config = match config::new("./config/p2p/config.toml") {
         Ok(c) => c,
-        Err(err) => panic!("Couldn't load config file: {:?}", err)
+        Err(err) => panic!("Couldn't load config file: {:?}", err),
     };
 
     // basic printing until logger is introduced
-    println!("Found config file:\n- listen_address: {}\n--------", p2p_config.listen_address);
+    println!(
+        "Found config file:\n- listen_address: {}\n--------",
+        p2p_config.listen_address
+    );
 
     let p2p_service = DevP2PService::new(p2p_config);
     match p2p_service.start().await {
