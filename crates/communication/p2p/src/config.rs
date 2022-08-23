b6a6eb1b196ec6691f2fd0102ae6ed960cb140fe
Wolmin <lamonos123@gmail.com>
2022-08-23 15:18:37 +0200

    Add config.dist file and implement configuration loading

--- crates/communication/p2p/src/config.rs
+++ crates/communication/p2p/src/config.rs
@@ -1,5 +1,15 @@
+use config_file::{FromConfigFile, ConfigFileError};
+use serde::Deserialize;
+
 /// P2P service configuration
+#[derive(Deserialize)]
 pub struct P2PConfiguration {
     /// IP address to listen for incoming connections.
     pub listen_address: String,
+}
+
+pub fn new(path: &str) -> Result<P2PConfiguration, ConfigFileError> {
+    println!("Trying to add config from path {}", path);
+
+    return P2PConfiguration::from_config_file(std::path::Path::new(path));
 }
--- crates/quantum-metachain/src/main.rs
+++ crates/quantum-metachain/src/main.rs
@@ -1,12 +1,16 @@
+use qmc_p2p::config;
 use qmc_p2p::config::P2PConfiguration;
 use qmc_p2p::service::{DevP2PService, P2PService};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let p2p_config = P2PConfiguration {
-        // TODO JEQB-79 read listen address from config
-        listen_address: "/ip4/0.0.0.0/tcp/0".to_string(),
+    let p2p_config = match config::new("./config/p2p/config.toml") {
+        Ok(c) => c,
+        Err(err) => panic!("Couldn't load config file: {:?}", err)
     };
+
+    // basic printing until logger is introduced
+    println!("Found config file:\n- listen_address: {}\n--------", p2p_config.listen_address);
 
     let p2p_service = DevP2PService::new(p2p_config);
     match p2p_service.start().await {
