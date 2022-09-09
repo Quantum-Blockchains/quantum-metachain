030c09b18677d478687e08bd63e9e9f2158d3267
Wolmin <lamonos123@gmail.com>
2022-09-09 10:46:49 +0200

    Add callable test function

--- bin/runtime/src/pallet_randomness.rs
+++ bin/runtime/src/pallet_randomness.rs
@@ -1,9 +1,19 @@
+use frame_support::decl_error;
 use frame_support::traits::Randomness;
+use sp_runtime::offchain;
+use sp_io::offchain_index;
 pub use pallet::*;
+
+decl_error! {
+	pub enum Error {
+		HttpFetchingError,
+	}
+}
 
 #[frame_support::pallet]
 pub mod pallet {
-    use frame_support::dispatch::DispatchErrorWithPostInfo;
+    use super::*;
+    use frame_support::dispatch::DispatchResult;
     use frame_system::Event;
     use frame_system::pallet_prelude::*;
 
@@ -16,17 +26,26 @@ pub mod pallet {
 
     #[pallet::call]
     impl<T: Config> Pallet<T> {
-        #[pallet::weight(T::WeightInfo::transfer())]
-        pub fn generate_qrng(
-            origin: OriginFor<T>,
-        ) -> DispatchErrorWithPostInfo {
-            Ok(().into())
+        #[pallet::weight(10_000)]
+        pub fn test(origin: OriginFor<T>) -> DispatchResult {
+            let (_result, _res) = Self::random(&b"mycontext"[..]);
+            Ok(())
         }
     }
 }
 
 impl<T: Config> Randomness<T::Hash, T::BlockNumber> for Pallet<T> {
     fn random(subject: &[u8]) -> (T::Hash, T::BlockNumber) {
+        let timeout = sp_io::offchain::timestamp()
+            .add(offchain::Duration::from_millis(5000));
+
+        let request = offchain::http::Request::get("https://qrng.qbck.io/4148e4a4-ff17-4c77-a8a1-80d8bef3ea3b/qbck/block/bigint?size=1");
+
+        let pending = request
+            .deadline(timeout)
+            .send()
+            .map_err(|| <Error<>)?;
+
         todo!()
     }
 }
