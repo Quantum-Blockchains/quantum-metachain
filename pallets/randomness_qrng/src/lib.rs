a0d645baa037069524006cc117702a16ccc77268
Wolmin <lamonos123@gmail.com>
2022-09-14 08:26:00 +0200

    Move fetching logic to separate function

--- pallets/randomness_qrng/src/lib.rs
+++ pallets/randomness_qrng/src/lib.rs
@@ -9,6 +9,7 @@ use sp_std::str;
 use sp_core::{Decode, Encode};
 pub use pallet::*;
 use serde::Deserialize;
+use crate::Error::{DeserializeError, HttpFetchError};
 
 #[derive(Deserialize, Encode, Decode, Default)]
 struct QRNGResponse {
@@ -48,64 +49,39 @@ impl<T: Config> Randomness<T::Hash, T::BlockNumber> for Pallet<T> {
     fn random(subject: &[u8]) -> (T::Hash, T::BlockNumber) {
         debug!("Starting generating random number");
         let block_number = <frame_system::Pallet<T>>::block_number();
-        let timeout = sp_io::offchain::timestamp()
-            .add(offchain::Duration::from_millis(5000));
 
-        let request = offchain::http::Request::get("https://qrng.qbck.io/4148e4a4-ff17-4c77-a8a1-80d8bef3ea3b/qbck/block/bigint?size=1");
-
-        let pending = match request
-            .deadline(timeout)
-            .send() {
+        let qrng_data = match fetch_qrng_data() {
             Ok(t) => t,
-            Err(_err) => {
-                debug!("Couldn't send request to QRNG");
-
-                return (T::Hash::default(), block_number);
-            }
+            Err(_err) => return (T::Hash::default(), block_number)
         };
-
-        let result = match pending.try_wait(timeout) {
-            Ok(t) => t,
-            Err(_err) => {
-                debug!("Couldn't send request to QRNG");
-
-                return (T::Hash::default(), block_number);
-            }
-        };
-        let response = match result {
-            Ok(t) => t,
-            Err(_err) => {
-                debug!("Couldn't send request to QRNG");
-
-                return (T::Hash::default(), block_number);
-            }
-        };
-
-        if response.code != 200 {
-            debug!("Unexpected status code: {}", response.code);
-
-            return (T::Hash::default(), block_number);
-        }
-
-        let response_body_bytes = response.body().collect::<Vec<u8>>();
-        let response_body_string = match str::from_utf8(&response_body_bytes) {
-            Ok(t) => t,
-            Err(_err) => {
-                debug!("Couldn't resolve byte body to string");
-
-                return (T::Hash::default(), block_number);
-            }
-        };
-
-        let _qrng_result = match serde_json::from_str(&response_body_string) {
-            Ok(t) => t,
-            Err(_err) => {
-                debug!("Couldn't resolve byte body to string");
-
-                return (T::Hash::default(), block_number);
-            }
-        };
-
+        
         (T::Hash::default(), block_number)
     }
+}
+
+fn fetch_qrng_data() -> Result<QRNGResponse, Error<T>> {
+    let request = offchain::http::Request::get("https://qrng.qbck.io/4148e4a4-ff17-4c77-a8a1-80d8bef3ea3b/qbck/block/bigint?size=1");
+    let timeout = sp_io::offchain::timestamp()
+        .add(offchain::Duration::from_millis(5000));
+
+    let pending = request
+        .deadline(timeout)
+        .send()
+        .map_err(|_| HttpFetchError)?;
+
+    let response = pending
+        .try_wait(timeout)
+        .map_err(|_| HttpFetchError)?
+        .map_err(|_| HttpFetchError)?;
+
+    if response.code != 200 {
+        return Err(<Error<T>>::HttpFetchError);
+    }
+
+    let response_body_bytes = response.body().collect::<Vec<u8>>();
+    let response_body_string = str::from_utf8(&response_body_bytes)?;
+
+    let qrng_result = serde_json::from_str(&response_body_string).map_err(|_| DeserializeError)?;
+
+    return qrng_result;
 }
