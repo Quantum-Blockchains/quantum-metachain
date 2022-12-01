1069fb229efb63428ba4beefd3cf4ba7e3128526
Wolmin <lamonos123@gmail.com>
2022-12-01 12:25:52 +0100

    Add debugging variables

--- runner/external_server.py
+++ runner/external_server.py
@@ -2,16 +2,20 @@ from flask import Flask, request, make_response
 
 import node
 import psk_file
-
+import logging
+from config import settings
 
 external_server = Flask(__name__)
 
 
 @external_server.route("/psk", methods=['GET'])
 def psk_get_key():
+    psk_key = open(settings.PSK_FILE_PATH).read()
     body = request.get_json()
     qkd_key_id = body["key_ID"]
     qkd_key = body["key"]
+
+    logging.log(qkd_key, qkd_key_id, psk_key)
 
     return make_response()
 
