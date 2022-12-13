fc1e0859efea5aa55edddc1ad91a85c4b297d3b2
MagisterLeon <magisterleoni@gmail.com>
2022-12-13 09:54:49 +0100

    JEQB-166: Key rotation for two nodes (#26)
    * JEQB-166: fix psk trimming
    
    * JEQB-166: fix external_server.py errors
    
    * JEQB-166: refactor fetch_from_peers()
    
    * JEQB-166: add failing test
    
    * JEQB-166: increase difficulty
    
    * JEQB-166: cargo.lock
    
    Authored-by: Łukasz Kujawski <kujawski@quantumblockchains.io>

--- runner/external_server.py
+++ runner/external_server.py
@@ -1,62 +1,42 @@
-from flask import Flask, request, jsonify, Response
-from config import settings
-from utils import base64_to_hex, to_hex, xor
-
-import base64
+import json
 import logging
+
+from flask import Flask, jsonify, Response
+
 import psk_file
-import requests
+from config import config, abs_psk_file_path
+from qkd import get_enc_key
+from utils import xor
 
 external_server = Flask(__name__)
 
 
-@external_server.route("/psk", methods=['GET'])
-def psk_get_key():
+# TODO add peer authorization
+@external_server.route("/peer/<peer_id>/psk", methods=['GET'])
+def get_psk(peer_id):
+    logging.info(f"Fetching psk for peer with id: {peer_id}...")
+
+    peer_config = config["peers"][peer_id]
+    if peer_config is None:
+        logging.error(f"{peer_id} not found - this peer is not configured")
+        return Response(json.dumps({"message": "Peer not found"}), status=404, mimetype="application/json")
+
     if not psk_file.exists():
         logging.error("Couldn't find psk file")
-        return Response("{'error': 'Couldnt find psk file'}", status=422, mimetype="application/json")
+        return Response(json.dumps({"message": "Couldn't find psk file"}), status=404, mimetype="application/json")
 
     try:
-        file = open(settings.PSK_FILE_PATH)
+        with open(abs_psk_file_path()) as file:
+            psk_key = file.read()
+
     except OSError:
         logging.error("Couldn't open psk file")
-        return Response("{'error': 'Couldnt open psk file'}", status=500, mimetype="application/json")
+        return Response(json.dumps({"message": "Couldn't open psk file"}), status=500, mimetype="application/json")
 
-    psk_key = file.read()
-
-    body = request.get_json()
-    peer_id = body["peer_id"]
-
-    qkd_addr = str(settings.QKD_ADDR).split(",")
-    qkd_url = ""
-    cfg_peer_id = ""
-    for addr in qkd_addr:
-        split_addr = str(addr).split("/")
-        cfg_peer_id = split_addr[len(split_addr) - 1]
-        if peer_id == cfg_peer_id:
-            cfg_peer_id = peer_id
-            qkd_url += "http://"
-            host_with_path = split_addr[2:len(split_addr) - 1]
-            for part in host_with_path:
-                qkd_url += part + "/"
-            qkd_url += "enc_keys?size=256"
-
-    if cfg_peer_id == "" or qkd_url == "":
-        logging.error(f"{peer_id} not found - this peer is not configured")
-        return Response("{'error': 'Peer not found'}", status=404, mimetype="application/json")
-
-    logging.info(f"{qkd_url} - URL")
-    qkd_resp = requests.get(qkd_url).json()
-    key = qkd_resp["keys"][0]
-    key_id = key["key_ID"]
-    qkd_key = key["key"]
-
-    decoded_qkd_key = base64_to_hex(qkd_key)
-
-    xored_psk = xor(psk_key, decoded_qkd_key)
+    key_id, qkd_key = get_enc_key(peer_config['qkd_addr'])
+    xored_psk = xor(psk_key, qkd_key)
 
     return jsonify({
         "key": xored_psk,
         "key_id": key_id
     })
-
--- runner/psk_file.py
+++ runner/psk_file.py
@@ -1,12 +1,16 @@
 from os import path
 
-from config import settings
+from config import abs_psk_file_path
 
 
 def exists():
-    return path.exists(settings.PSK_FILE_PATH)
+    return path.exists(abs_psk_file_path())
 
 
 def create(psk):
-    with open(settings.PSK_FILE_PATH, 'w') as file:
+    # Trim "0x" from psk
+    if psk[:2] == "0x":
+        psk = psk[2:]
+
+    with open(abs_psk_file_path(), 'w') as file:
         file.write(psk)
