8168759f346bc7594d23b9bceb5552dfbd9917b5
zedleppelin74 <agata.kam@hotmail.com>
2022-12-14 10:51:56 +0100

    Sign psk upon fetching

--- runner/local_server.py
+++ runner/local_server.py
@@ -5,6 +5,7 @@ from flask import Flask, request, make_response
 import node
 import psk_file
 from psk import fetch_from_qrng, fetch_from_peers
+from auth import sign
 
 local_server = Flask(__name__)
 
@@ -16,7 +17,7 @@ def rotate_pre_shared_key():
     logging.info("Rotating pre-shared key...")
     body = request.get_json()
     is_local_peer = body["is_local_peer"]
-    psk = fetch_from_qrng() if is_local_peer else fetch_from_peers()
+    psk = sign(fetch_from_qrng()) if is_local_peer else fetch_from_peers()
     psk_file.create(psk)
 
     node.node_service.current_node.restart()
