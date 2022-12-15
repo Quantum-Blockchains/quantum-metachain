47ba3dcecd6bb6fa24aa56577ff31a693eb15480
zedleppelin74 <agata.kam@hotmail.com>
2022-12-15 15:11:09 +0100

    Deleted unnecessary vars

--- runner/auth.py
+++ runner/auth.py
@@ -7,11 +7,7 @@ from config import abs_node_key_file_path
 def sign(file_abs_path):
     try:
         private_key = ed25519.Ed25519PrivateKey.from_private_bytes(open(abs_node_key_file_path(), 'rb').read())
-
         signed_file = private_key.sign(open(file_abs_path, 'rb').read())
-
-        public_key = private_key.public_key()
-        public_key.verify(signed_file, open(file_abs_path, 'rb').read())
 
     except Exception as e:
         logging.error(str(e))
