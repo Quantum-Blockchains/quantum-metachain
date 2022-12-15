73904c981b3a7358709f4656d86abc03d4f51d2a
zedleppelin74 <agata.kam@hotmail.com>
2022-12-15 20:34:47 +0100

    Fixed signing fn

--- runner/auth.py
+++ runner/auth.py
@@ -5,12 +5,12 @@ from cryptography.hazmat.primitives.asymmetric import ed25519
 from config import abs_node_key_file_path
 
 
-def sign(file_abs_path):
+def sign(data):
     try:
         private_key = ed25519.Ed25519PrivateKey.from_private_bytes(open(abs_node_key_file_path(), 'rb').read())
-        signed_file = private_key.sign(open(file_abs_path, 'rb').read())
+        signed_data = private_key.sign(data)
 
     except Exception as e:
         logging.error(str(e))
 
-    return signed_file
+    return signed_data
