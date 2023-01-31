af3bfce3f407ef3eaa57bdf25b3ac5a8829640c3
Wolmin <lamonos123@gmail.com>
2023-01-31 11:32:48 +0100

    Test qkd.py

--- runner/qkd.py
+++ runner/qkd.py
@@ -1,15 +1,25 @@
 import requests
+import logging
 from utils import base64_to_hex
+from werkzeug.exceptions import HTTPException
+import validators
 
 
 def get_enc_key(url):
     qkd_url = f"{url}/enc_keys?size=256"
+    if not validators.url(qkd_url):
+        raise HTTPException
+
     response = requests.get(qkd_url).json()
+
     return _unwrap_response(response)
 
 
 def get_dec_key(url, key_id):
     qkd_url = f"{url}/dec_keys?key_ID={key_id}"
+    if not validators.url(qkd_url):
+        raise HTTPException
+
     response = requests.get(qkd_url).json()
 
     return _unwrap_response(response)
---
+++ runner/test_qkd.py
@@ -0,0 +1,109 @@
+import base64
+
+from qkd import get_enc_key, get_dec_key
+from werkzeug.exceptions import HTTPException
+
+
+def new_test_case(should_pass, url, response_body):
+    return {
+        "should_pass": should_pass,
+        "url": url,
+        "response_body": response_body,
+    }
+
+
+def test_get_enc_key_success(requests_mock):
+    enc_url = "http://correct.url"
+    expected_url = f"{enc_url}/enc_keys?size=256"
+    expected_resp = {
+        "keys": [
+            {
+                "key_ID": "key_ID",
+                "key": "EjQ="  # encoded 1234
+            }
+        ]
+    }
+    requests_mock.get(expected_url, json=expected_resp)
+
+    key_id, decoded_key = get_enc_key(enc_url)
+    assert key_id == "key_ID"
+    assert decoded_key == "0x1234"
+
+
+def test_get_enc_key_empty_url(requests_mock):
+    enc_url = ""
+    expected_url = f"{enc_url}/enc_keys?size=256"
+    expected_resp = {}
+
+    requests_mock.get(expected_url, json=expected_resp)
+
+    try:
+        _, _ = get_enc_key(enc_url)
+    except HTTPException:
+        return
+
+    raise Exception("Expected error, didn't receive one")
+
+
+def test_get_enc_key_invalid_url(requests_mock):
+    enc_url = "http:/invalid schema"
+    expected_url = f"{enc_url}/enc_keys?size=256"
+    expected_resp = {}
+
+    requests_mock.get(expected_url, json=expected_resp)
+
+    try:
+        _, _ = get_enc_key(enc_url)
+    except HTTPException:
+        return
+
+    raise Exception("Expected error, didn't receive one")
+
+
+def test_get_dec_key_success(requests_mock):
+    key_id = "key_ID"
+    dec_url = "http://correct.url"
+    expected_url = f"{dec_url}/dec_keys?key_ID={key_id}"
+    expected_resp = {
+        "keys": [
+            {
+                "key_ID": key_id,
+                "key": "QyE="  # encoded 4321
+            }
+        ]
+    }
+    requests_mock.get(expected_url, json=expected_resp)
+
+    key_id_response, decoded_key = get_dec_key(dec_url, key_id)
+    assert key_id_response == key_id
+    assert decoded_key == "0x4321"
+
+
+def test_get_dec_key_empty_url(requests_mock):
+    dec_url = ""
+    expected_url = f"{dec_url}/dec_keys?key_ID="
+    expected_resp = {}
+
+    requests_mock.get(expected_url, json=expected_resp)
+
+    try:
+        _, _ = get_enc_key(dec_url)
+    except HTTPException:
+        return
+
+    raise Exception("Expected error, didn't receive one")
+
+
+def test_get_dec_key_invalid_url(requests_mock):
+    dec_url = "http:/invalid schema"
+    expected_url = f"{dec_url}/dec_keys?key_ID="
+    expected_resp = {}
+
+    requests_mock.get(expected_url, json=expected_resp)
+
+    try:
+        _, _ = get_enc_key(dec_url)
+    except HTTPException:
+        return
+
+    raise Exception("Expected error, didn't receive one")
