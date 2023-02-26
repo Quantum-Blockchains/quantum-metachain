e6ef0aa71705e878c6069a2bb98e9c3f4a0cf4c4
Wolmin <lamonos123@gmail.com>
2023-02-26 17:51:50 +0100

    Use onetimepad instead of xoring

--- runner/core/onetimepad.py
+++ runner/core/onetimepad.py
@@ -1,21 +1,15 @@
-def xor_bytes(a, b):
-    """Performs a bitwise XOR of two strings. Returns a byte string."""
-    return bytes([ord(x) ^ ord(y) for x, y in zip(a, b)])
+import onetimepad
 
 
 def encrypt(plaintext, key):
     """
     Encrypts the plaintext using the specified key.
     """
-    bytetext = xor_bytes(plaintext, key)
-    return bytetext.decode("utf-8")
+    return onetimepad.encrypt(plaintext, key)
 
 
 def decrypt(ciphertext, key):
     """
     Decrypts the ciphertext using the specified key.
-    Please note that encrypt returns byte string, but encrypting function should only accept string as an argument
-    (because of ord() in xor_bytes()).
     """
-    bytetext = xor_bytes(ciphertext, key)
-    return bytetext.decode("utf-8")
+    return onetimepad.decrypt(ciphertext, key)
