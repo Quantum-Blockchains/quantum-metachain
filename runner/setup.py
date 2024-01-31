48e31c9d6f574c7a0bebff5591245211ea31872b
kostiask <kostiaskopec@gmail.com>
2024-01-31 16:44:20 +0100

    use argparse

--- runner/setup.py
+++ runner/setup.py
@@ -4,26 +4,31 @@ from os import path, mkdir, remove
 import ed25519
 from common.logger import log
 import requests
+import argparse
+import pathlib
+from urllib.parse import urlparse
 
-ROOT_DIR = path.abspath(path.dirname(__file__) + "/..")
 
-config = {}
+def url_type(arg):
+    url = urlparse(arg)
+    if all((url.scheme, url.netloc)):
+        return arg
+    raise argparse.ArgumentTypeError('Invalid URL')
 
 
-def enter_port(message, default_port):
-    while True:
-        try:
-            port_str = input(f'{message} (Default port number is {default_port}): ')
-            if port_str == "":
-                return default_port
-            port = int(port_str)
-            if 1 <= port <= 65535:
-                return port
-            else:
-                raise ValueError
-        except ValueError:
-            print('Error: this is NOT a VALID port number. The port must be a number from 1 to 65535.')
-            continue
+def qrng_type(arg):
+    # TODO JEQB-270/Check entered qrng api key
+    return arg
+
+
+def peer_type(arg):
+    #TODO prowierka
+    return arg
+
+
+def node_key_type(arg):
+    # TODO JEQB-269/Check enter node key
+    return arg
 
 
 def data_exchange_eith_the_selected_node(address):
@@ -37,98 +42,78 @@ def data_exchange_eith_the_selected_node(address):
         "server_addr": address
     }
 
+ROOT_DIR = path.abspath(path.dirname(__file__) + "/..")
 
-print("Create a configuration for a runner node.")
+config = {}
 
-while True:
-    try:
-        tmp = int(input("Node key. If you already have a node key, enter 1 if you don't hae one, enter2: "))
-        if tmp == 1 or tmp == 2:
-            break
-        else:
-            raise ValueError
-    except ValueError:
-        print('ERROR: an option that does not exist has been selected. Only 1 or 2 can be selected.')
-        continue
+parser = argparse.ArgumentParser(
+    prog='runner/setuo.py',
+    description='Quantum Blockchains configuration of runner.'
+)
+
+parser.add_argument('--local-server-port', dest='local_server_port', default=5001, type=int, nargs='?',
+                    help='Local server port.')
+
+parser.add_argument('--external-server-port', dest='external_server_port', default=5002,
+                    type=int, nargs='?', help='External server port')
+
+parser.add_argument('--node-http-rpc-port', dest='node_http_rpc_port', default=9933, type=int, nargs='?',
+                    help='Node http rpc port.')
+
+parser.add_argument('--key-rotation-time', dest='key_rotation_time', default=50, type=int, nargs='?',
+                    help='Key rotation time.')
+
+parser.add_argument('--recovery-check-interval', dest='recovery_check_interval', default=50, type=int,
+                    nargs='?', help='Recovery check interval.')
+
+parser.add_argument('--qrng-api-key', dest='qrng_api_key', default=50, type=qrng_type, required=True,
+                    help='Qrng api key.')
+
+group_args_of_node_key = parser.add_mutually_exclusive_group()
+group_args_of_node_key.add_argument("--node-key-file", dest='path_to_node_key', type=pathlib.Path,
+                                    help="Path to node key.")
+group_args_of_node_key.add_argument("--node-key", dest='node_key', type=node_key_type, help="Node key.")
+
+parser.add_argument('--url', dest='boot_url', type=url_type, required=True, help='Boot url.')
+
+parser.add_argument('--peers', dest='peers', type=peer_type, nargs='*', required=True, help='Peers.')
+
+args = parser.parse_args()
+
+print(args.boot_url)
+print(args.peers)
 
 config_dir = path.join(ROOT_DIR, ".config")
 if not path.exists(config_dir):
     mkdir(config_dir)
 
-match tmp:
-    case 1:
-        str_tmp = input("Enter node key (64 hex characters): ")
-        # TODO JEQB-269/Check enter node key
-        open(path.join(config_dir, "tmp_key"), "w").write(str_tmp)
-    case 2:
-        signing_key, _ = ed25519.create_keypair()
-        open(path.join(config_dir, "tmp_key"), "wb").write(signing_key.to_ascii(encoding="hex"))
+if args.path_to_node_key is not None:
+    print("1")
+elif args.node_key is not None:
+    print("2")
+    open(path.join(config_dir, "tmp_key"), "w").write(args.node_key)
+else:
+    print("3")
+    signing_key, _ = ed25519.create_keypair()
+    open(path.join(config_dir, "tmp_key"), "wb").write(signing_key.to_ascii(encoding="hex"))
 
-args = "target/release/qmc-node" + " key" + " inspect-node-key" + " --file " + path.join(config_dir, "tmp_key")
-peer_id = subprocess.check_output(args, shell=True, executable="/bin/bash", stderr=subprocess.STDOUT)
+args_sub = "target/release/qmc-node" + " key" + " inspect-node-key" + " --file " + path.join(config_dir, "tmp_key")
+peer_id = subprocess.check_output(args_sub, shell=True, executable="/bin/bash", stderr=subprocess.STDOUT)
 peer_id = peer_id.decode('utf-8').strip()
-
-remove(path.join(config_dir, "tmp_key"))
-
 node_config_dir = path.join(config_dir, peer_id)
 
-while True:
-    try:
-        key_rotation_time = int(input("Enter key rotation time: "))
-        if key_rotation_time > 0:
-            break
-        else:
-            raise ValueError
-    except ValueError:
-        print('ERROR: enter the wrong value. Enter a number.')
-        continue
-
-qrng_api_key = input("Enter qrng api key: ")
-# TODO JEQB-270/Check entered qrng api key
-
-while True:
-    try:
-        recovery_check_interval = int(input("Enter recovery check interval: "))
-        if key_rotation_time > 0:
-            break
-        else:
-            raise ValueError
-    except ValueError:
-        print('ERROR: enter the wrong value. Enter a number.')
-        continue
-
-config["__type__"] = "Config"
-config["local_peer_id"] = peer_id
-config["local_server_port"] = enter_port("Enter local server port", 5001)
-config["external_server_port"] = enter_port("Enter external server port", 5002),
-config["node_http_rpc_port"] = enter_port("Enter node http rpc port server port", 9933)
-config["psk_file_path"] = path.join(f'../.config/{peer_id}', "psk")
-config["psk_sig_file_path"] = path.join(f'../.config/{peer_id}', "psk_sig")
-config["node_key_file_path"] = path.join(f'../.config/{peer_id}', "node_key")
-config["key_rotation_time"] = key_rotation_time
-config["qrng_api_key"] = qrng_api_key
-config["recovery_check_interval"] = recovery_check_interval
-config["peers"] = {}
-
+remove(path.join(config_dir, "tmp_key"))
 # TODO tut nado sdzielac request do jakiegos istnioncego noda zebys on podal do ktorych nodow trzeba poloczyc sie
 # TODO sdzielac prowierku <peer_id>
-peers = []
-while True:
-    try:
-        list_of_peers = input("Enter peers (<peer_id>, <peer_id>, ...): ")
-        peers = list_of_peers.split(", ")
-        break
-    except ValueError:
-        print('ERROR: enter the wrong value. ')
-        continue
 
-# TODO sdzielac prowierku
-addr = input("Enter one external_server address: ")
+
 addresses = {}
-addresses[addr] = False
 
+addresses[args.boot_url] = False
 
-for peer in peers:
+peers_for_config = {}
+
+for peer in args.peers:
     qwerty = True
     while qwerty:
         for a in list(addresses.keys()):
@@ -143,7 +128,7 @@ for peer in peers:
                 response_body = search_peer_response.json()
                 if response_body["found"]:
                     peer_info = data_exchange_eith_the_selected_node(response_body["external_server_address"])
-                    config["peers"][peer] = peer_info
+                    peers_for_config[peer] = peer_info
                     print(peer_info)
                     qwerty = False
                     break
@@ -154,6 +139,22 @@ for peer in peers:
                             continue
                         else:
                             addresses[x] = False
+
+config = {
+    "__type__": "Config",
+    "local_peer_id": peer_id,
+    "local_server_port": args.local_server_port,
+    "external_server_port": args.external_server_port,
+    "node_http_rpc_port": args.node_http_rpc_port,
+    "psk_file_path": path.join(f'../.config/{peer_id}', "psk"),
+    "psk_sig_file_path": path.join(f'../.config/{peer_id}', "psk_sig"),
+    "node_key_file_path": path.join(f'../.config/{peer_id}', "node_key"),
+    "node_logs_path": "",
+    "key_rotation_time": args.key_rotation_time,
+    "qrng_api_key": args.qrng_api_key,
+    "recovery_check_interval": args.recovery_check_interval,
+    "peers": peers_for_config
+  }
 
 if not path.exists(node_config_dir):
     mkdir(node_config_dir)
