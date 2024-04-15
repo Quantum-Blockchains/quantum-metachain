b08436151f83c76b62d366eceab705b60d26738f
kostia <kostiaskopec@gmail.com>
2024-04-15 21:09:09 +0200

    update external_server.py 2

--- runner/web/external_server.py
+++ runner/web/external_server.py
@@ -124,7 +124,7 @@ def data_qkd_exchange():
             "provider": "etsi014",
             "url": common.config.config_service.config.local_qkd_url + "/api/v1/keys/" + qkd_name,
             "client_cert_path": common.config.to_absolute("../certificates/qbck-client.crt"),
-            "cert_key_path": "../certificates/qbck-client.key"
+            "cert_key_path":  common.config.to_absolute("../certificates/qbck-client.key")
         },
         "server_addr": server_addr
     }
